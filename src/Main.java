5ce1f747147ac5b4aa48886f95df2fe6bf280353
22mariahelena <up202406524@up.pt>
2025-02-23 21:54:01 +0000

    Second commit Execução da classe "Main"

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Cilindro cilindro = new Cilindro();
+        cilindro.setRaio(2);
+        cilindro.setAltura(3);
 
+        Cilindro cilindro1 = new Cilindro();
+        cilindro1.setRaio(3);
+        cilindro1.setAltura(4);
+
+        System.out.println("Volume: " + cilindro.calcularVolume());
+        System.out.println("Área: " + cilindro.calcularArea());
+
+        System.out.println("Volume: " + cilindro1.calcularVolume());
+        System.out.println("Área: " + cilindro1.calcularArea());
     }
 }
